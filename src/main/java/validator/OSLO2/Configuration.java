5ba620e6ebca13b1397ba710c4b7c3e5a8be4a96
ddvlanck <dwight.vanlancker@hotmail.com>
2019-10-25 13:52:35 +0200

    Updated URL for shacl config Running web app works again

--- src/main/java/validator/OSLO2/Configuration.java
+++ src/main/java/validator/OSLO2/Configuration.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Configuration {
     //private String apConfig = "https://data.vlaanderen.be/src/shacl-validator-config.json";
-    private String apConfig = "https://github.com/Informatievlaanderen/Data.Vlaanderen.be/blob/production/config/shacl-validator-config.json";
+    private String apConfig = "https://raw.githubusercontent.com/Informatievlaanderen/Data.Vlaanderen.be/production/config/shacl-validator-config.json";
     private static final String AP_CONFIG = "AP_CONFIG";
     private final LoadingCache<String, Map<String, APModel>> cache;
 
